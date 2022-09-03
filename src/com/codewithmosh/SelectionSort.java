5d5f7aed45356b9834a228d65af6f2486b9f6e72
Shewket <1842065771@qq.com>
2022-09-03 09:49:05 +0800

    Sorting Algorithms

---
+++ src/com/codewithmosh/SelectionSort.java
@@ -0,0 +1,23 @@
+package com.codewithmosh;
+
+public class SelectionSort{
+    public void sort(int[] array){
+        for (var i = 0; i < array.length; i++){
+            swap(array, findMinIndex(array, i), i);
+        }
+    }
+
+    private int findMinIndex(int[] array, int index){
+            var minIndex = index;
+            for (var j = index; j < array.length; j++)
+                if (array[j] < array[minIndex])
+                    minIndex = j;
+            return minIndex;
+    }
+
+    private void swap(int[] array, int index1, int index2){
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
