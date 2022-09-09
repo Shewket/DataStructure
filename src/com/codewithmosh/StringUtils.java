6757e590871aa2a266588428f369133d35373ab3
Shewket <1842065771@qq.com>
2022-09-09 22:52:38 +0800

    END

--- src/com/codewithmosh/StringUtils.java
+++ src/com/codewithmosh/StringUtils.java
@@ -1,6 +1,6 @@
 package com.codewithmosh;
 
-import java.util.Locale;
+import java.util.*;
 
 public class StringUtils {
     public static int countVowels(String str){
@@ -11,7 +11,7 @@ public class StringUtils {
         String vowels = "aeiou";
         for (var ch : str.toLowerCase().toCharArray())
             //  if (vowels.contains(Character.toString(ch)))
-            if (vowels.indexOf(ch) != -1)
+             if (vowels.indexOf(ch) != -1)
                 count++;
 
         return count;
@@ -26,5 +26,151 @@ public class StringUtils {
             reversed.append(str.charAt(i)); //  O(1)
 
         return reversed.toString();
+    }
+
+    //  Trees are beautiful
+    //  beautiful are Tress
+    public static String reverseWords(String sentence){
+        if (sentence == null)
+            return "";
+        String[] words = sentence.trim().split(" ");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+//        StringBuilder reversed = new StringBuilder();
+//        for (var i = words.length; i >= 0; i--)
+//            reversed.append(words[i] + " ");
+//
+//        return reversed.toString().trim();
+    }
+
+    //  ABCD -> DABC
+    //       -> CDAB
+    //       -> BCDA
+    //       -> ABCD
+    //  ABCDABCD
+    public static boolean areRotation(String str1, String str2){
+
+        if (str1 == null || str2 == null)
+            return false;
+
+        return (str1.length() == str2.length() && (str1 + str1).contains(str2));
+
+//        if (str1.length() != str2.length())
+//            return false;
+//
+//        if (!(str1 + str2).contains(str2))
+//            return false;
+//
+//        return true;
+    }
+
+    public static String removeDuplicates(String str){
+        if (str == null)
+            return "";
+
+        StringBuilder output = new StringBuilder();
+        Set<Character> seen = new HashSet<>();
+        for(var ch : str.toCharArray()){
+            if (!seen.contains(ch)){
+                seen.add(ch);
+                output.append(ch);
+            }
+        }
+        return output.toString();
+    }
+
+    public static char getMaxOccuringChar(String str){
+//        Map<Character, Integer> frequencies = new HashMap<>();
+//        for (var ch : str.toCharArray()){
+//            if (frequencies.containsKey(ch))
+//                frequencies.replace(ch, frequencies.get(ch) + 1);
+//            else
+//                frequencies.put(ch, 1);
+        if (str == null || str.isEmpty())
+            throw new IllegalArgumentException();
+        final int ASCII_SIZE = 256;
+        int[] frequencies = new int[ASCII_SIZE];
+        for (var ch : str.toCharArray())
+            frequencies[ch]++;
+
+        int max = 0;
+        char result = ' ';
+        for (var i = 0; i < frequencies.length; i++)
+            if (frequencies[i] > max){
+                max = frequencies[i];
+                result = (char) i;
+        }
+        return result;
+    }
+
+    public static String capitalize(String sentence){
+        if (sentence == null || sentence.trim().isEmpty())
+            return "";
+        String[] words = sentence
+                            .trim()
+                            .replaceAll(" +", " ")
+                            .split(" ");
+        for (var i = 0; i < words.length; i++)
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
+
+        return String.join(" ", words);
+    }
+
+    //  ABCD - CBDA
+    //  O(n log n)
+    public static boolean areAnagrams(String first, String second){
+        if (first == null || second == null)
+            return false;
+
+        //  O(n)
+        var array1 = first.toLowerCase().toCharArray();
+        //  O(n log n)
+        Arrays.sort(array1);
+
+        var array2 = second.toLowerCase().toCharArray();
+        Arrays.sort(array2);
+
+        return Arrays.equals(array1, array2);
+    }
+
+    //  O(n)
+    public static boolean areAnagram2(String first, String second){
+        if (first == null || second == null)
+            return false;
+        final int ENGLISH_ALPHABET = 26;
+        int[] frequencies = new int[ENGLISH_ALPHABET];
+
+        first = first.toLowerCase();
+        //  O(n)
+        for (var i = 0; i < first.length(); i++)
+            frequencies[first.charAt(i) - 'a']++;
+
+        second = second.toLowerCase();
+        //  O(n)
+        for (var i = 0; i < second.length(); i++){
+            var index = second.charAt(i) - 'a';
+            if (frequencies[index] == 0)
+                return false;
+            frequencies[index]--;
+        }
+        return true;
+    }
+
+    //  ABBA, MADAM
+    //  l  r
+    public static boolean isPalindrome(String word){
+//        var input = new StringBuilder(word);
+//        input.reverse();
+//        return input.toString().equals(word);
+        if (word == null)
+            return false;
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right)
+            if (word.charAt(left++) != word.charAt(right--))
+                return false;
+
+        return true;
     }
 }
